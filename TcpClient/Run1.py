b19c33d341e8a08b26949071c6a98c407c5ee089
BrainPicker-L <1500202278@qq.com>
2018-12-23 14:50:53 +0800

    12.23

--- TcpClient/Run1.py
+++ TcpClient/Run1.py
@@ -4,7 +4,9 @@ from PyQt5.QtNetwork import QTcpSocket, QHostAddress
 from PyQt5.QtCore import QByteArray, QDataStream, QIODevice
 import sys
 
-
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 # 客户端与服务器端代码逻辑类似，注释省略
 
 class TcpClient(QWidget, Ui_MainWindow):
@@ -13,12 +15,20 @@ class TcpClient(QWidget, Ui_MainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.ui.radioButton.setChecked(True)
+
+
         self.tcpSocket = QTcpSocket(self)
+
         self.ui.connectButton.clicked.connect(self.buttonConnect)
-        #self.tcpSocket.connected.connect(self.sucessConnect)
+
         self.ui.sendButton.clicked.connect(self.sendInfoMessage)
+
+
+        # self.tcpSocket.connected.connect(self.sucessConnect)
         #self.tcpSocket.readyRead.connect(self.showMessage)
         #self.ui.closeButton.clicked.connect(self.closeConnect)
+
 
     def buttonConnect(self):
         ip = self.ui.IPLineEdit.text()
@@ -38,11 +48,30 @@ class TcpClient(QWidget, Ui_MainWindow):
         self.ui.sendText.clear()
 
     def sendInfoMessage(self):
+        if self.ui.radioButton.text() == '矩形':
+            if self.ui.radioButton.isChecked() == True:
+                radio = ',图形: 矩形'
+            else:
+                pass
+
+        if self.ui.radioButton_2.text() == "圆形":
+            if self.ui.radioButton_2.isChecked() == True:
+                radio = ',图形: 圆形'
+            else:
+                pass
+
+        x1 = ',x1: ' + self.ui.lineEdit_6.text()
+        y1 = ',y1: ' + self.ui.lineEdit_7.text()
+        x2 = ',x2: ' + self.ui.lineEdit_8.text()
+        y2 = ',y2: ' + self.ui.lineEdit_9.text()
+        R = ',R: ' + self.ui.lineEdit_10.text()
+        G = ',G: ' + self.ui.lineEdit_11.text()
+        B = ',B: '+ self.ui.lineEdit_12.text()
         name = self.ui.lineEdit_3.text()
         sno = self.ui.lineEdit_4.text()
         major = self.ui.lineEdit_5.text()
-        message = '姓名: ' + name + '学号: ' + sno + '专业: ' + major
-        print(message)
+        message = '姓名: ' + name + ',学号: ' + sno + ',专业: ' + major + radio + x1 + y1 + x2 + y2 + R + G + B
+
         self.communication = QByteArray()
         stream = QDataStream(self.communication, QIODevice.WriteOnly)
         stream.writeQString(message)
--- TcpServer/Run1.py
+++ TcpServer/Run1.py
@@ -4,6 +4,8 @@ from PyQt5.QtNetwork import QTcpSocket, QTcpServer, QHostAddress
 from PyQt5.QtCore import QByteArray, QDataStream, QIODevice
 from TcpClient.Run1 import TcpClient
 import sys,time
+from PyQt5.QtGui import QPainter, QColor, QBrush
+
 
 
 class TcpServer(QWidget, Ui_MainWindow):
@@ -12,16 +14,38 @@ class TcpServer(QWidget, Ui_MainWindow):
         super(TcpServer, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
+        self.message = ''
         self.tcpServer = QTcpServer(self)  # 指定父对象自动回收空间 监听套接字
         self.tcpSocket = QTcpSocket(self)  # 通信套接字
-
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+        self.radio = ''
+        self.R = 0
+        self.G = 0
+        self.B = 0
         self.tcpServer.listen(QHostAddress.Any, 8888)  # any默认绑定当前网卡的所有IP
         self.ui.showText.append('服务器初始化成功，等待客户端链接...    %s'%(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))))
         self.tcpServer.newConnection.connect(self.handleNewConnection)
-
 #        self.ui.sendButton.clicked.connect(self.sendMessage)
         self.ui.closeButton.clicked.connect(self.closeConnect)
+
+
+    def paintEvent(self,e):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QColor(int(self.R), int(self.G), int(self.B)))
+        if self.radio == '矩形':
+            qp.drawRect(430+int(self.x1), 110+int(self.y1), 0+int(self.x2), 0+int(self.y2))
+        elif self.radio == '圆形':
+            qp.drawEllipse(430+int(self.x1), 110+int(self.y1), 0+int(self.x2), 0+int(self.y2))
+        else:
+            qp.drawRect(430 + int(self.x1), 110 + int(self.y1), 0 + int(self.x2), 0 + int(self.y2))
+        qp.end()
+
+
+
 
     def handleNewConnection(self):
         self.tcpSocket = self.tcpServer.nextPendingConnection()  # 取出建立好链接的套接字
@@ -45,8 +69,21 @@ class TcpServer(QWidget, Ui_MainWindow):
     def showMessage(self):
         stream = QDataStream(self.tcpSocket)  # 发送数据是以QByteArray数据类型发送过来的，所以接收数据也应该以此接收
         stream.setVersion(QDataStream.Qt_5_10)  # 发送和接收数据以相同的编码形式传输
-        message = stream.readQString()  # 写入使用writeString, 对应读取使用readQString
-        self.ui.showText.append('%s   %s'%(message,time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))))
+        self.message = stream.readQString()  # 写入使用writeString, 对应读取使用readQString
+        self.ui.showText.append('%s   %s'%(self.message,time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))))
+        message_list = self.message.split(',')
+
+        self.radio = message_list[3][3:]
+        self.x1 = message_list[4][3:]
+        self.y1 = message_list[5][3:]
+        self.x2 = message_list[6][3:]
+        self.y2 = message_list[7][3:]
+
+        self.R = message_list[-3][3:]
+        self.G = message_list[-2][3:]
+        self.B = message_list[-1][3:]
+        print(message_list)
+
 
     def closeConnect(self):
         self.tcpSocket.disconnectFromHost()
